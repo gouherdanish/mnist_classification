4f97f6e9eeb2e708432bb801c51ace79eb92227d
Gouher Danish <gouherdanishtiitkgp@gmail.com>
2024-11-06 09:04:33 +0530

    Parametrized model file

--- models/mlp.py
+++ models/mlp.py
@@ -1,15 +1,18 @@
 import torch.nn as nn
 
+from constants import Constants, ModelParams
+
 class MLP(nn.Module):
     def __init__(self):
         super(MLP,self).__init__()
-        self.fc1 = nn.Linear(28*28, 128)
-        self.fc2 = nn.Linear(128, 64)
+        self.image_pixels = Constants.IMAGE_SIZE[0]*Constants.IMAGE_SIZE[1]
+        self.fc1 = nn.Linear(self.image_pixels, ModelParams.FC1_NEURONS)
+        self.fc2 = nn.Linear(ModelParams.FC1_NEURONS, ModelParams.FC2_NEURONS)
         self.relu = nn.ReLU()
-        self.output = nn.Linear(64, 10)
+        self.output = nn.Linear(ModelParams.FC2_NEURONS, ModelParams.OUTPUT_CLASSES)
     
     def forward(self, x):
-        x = x.view(-1,28*28)
+        x = x.view(-1,self.image_pixels)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         return self.output(x)
