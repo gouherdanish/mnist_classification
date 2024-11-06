e923909c30a001e36601c61151cef7980e3732cd
Gouher Danish <gouherdanishtiitkgp@gmail.com>
2024-11-06 09:05:04 +0530

    Added constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+class Constants:
+    IMAGE_SIZE = (28,28)
+    VALIDATION_SPLIT_FRAC = 0.1
+
+class HyperParams:
+    LEARNING_RATE = 0.01
+    BATCH_SIZE = 128
+
+class ModelParams:
+    FC1_NEURONS = 128
+    FC2_NEURONS = 64
+    OUTPUT_CLASSES = 10
