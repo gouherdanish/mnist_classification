24672a4e02f50c612c834d4144da705ec6ddde75
Gouher Danish <gouherdanishtiitkgp@gmail.com>
2024-11-07 08:34:14 +0530

    Updated constants file

--- constants.py
+++ constants.py
@@ -1,12 +1,17 @@
+import os
+
 class Constants:
     IMAGE_SIZE = (28,28)
     VALIDATION_SPLIT_FRAC = 0.1
 
+class PathConstants:
+    CUR_DIR = os.path.dirname(__file__)
+    MODEL_PATH = os.path.join(CUR_DIR,'out','mnist_mlp_weights.pth')
+
 class HyperParams:
-    LEARNING_RATE = 0.01
+    LEARNING_RATE = 0.001
     BATCH_SIZE = 128
 
 class ModelParams:
-    FC1_NEURONS = 128
-    FC2_NEURONS = 64
+    FC1_NEURONS = 512
     OUTPUT_CLASSES = 10
