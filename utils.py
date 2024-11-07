0d4815c772f2b3bc5503faa6171436f6afd20f0b
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-07 09:20:29 +0530

    added time tracking functionality

---
+++ utils.py
@@ -0,0 +1,11 @@
+import time
+
+class Utils:
+    def timeit(func):
+        def inner(*args,**kwargs):
+            start = time.time()
+            res = func(*args,**kwargs)
+            end = time.time()
+            print(f"Elapsed Time: {(end-start):.0f}s")
+            return res
+        return inner
