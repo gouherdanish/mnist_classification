6692f7ddeaf57e7178d3dff4daa0e6c98cdfd564
Gouher Danish <gouherdanishtiitkgp@gmail.com>
2024-11-07 08:35:30 +0530

    updated the model to have single hidden layer

--- models/mlp.py
+++ models/mlp.py
@@ -7,12 +7,10 @@ class MLP(nn.Module):
         super(MLP,self).__init__()
         self.image_pixels = Constants.IMAGE_SIZE[0]*Constants.IMAGE_SIZE[1]
         self.fc1 = nn.Linear(self.image_pixels, ModelParams.FC1_NEURONS)
-        self.fc2 = nn.Linear(ModelParams.FC1_NEURONS, ModelParams.FC2_NEURONS)
         self.relu = nn.ReLU()
         self.output = nn.Linear(ModelParams.FC2_NEURONS, ModelParams.OUTPUT_CLASSES)
     
     def forward(self, x):
         x = x.view(-1,self.image_pixels)
         x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
         return self.output(x)
