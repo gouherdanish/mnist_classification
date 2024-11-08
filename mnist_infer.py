fbb96a8ce47b8d748b59d5379415b7462f094b82
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-08 17:00:34 +0530

    separated main file for inference

--- mnist_infer.py
+++ mnist_infer.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 from constants import Constants, HyperParams, PathConstants
-from data.training_data import TestDataPreparation
+from data.test_data import TestDataPreparation
 from factory.model_factory import ModelFactory
 from inference import ModelInference
 
@@ -19,7 +19,10 @@ if __name__=='__main__':
 
     model_factory = ModelFactory()
     model = model_factory.select(model_type)
-    model.load_state_dict(torch.load(PathConstants.MODEL_PATH))
+    state_dict = torch.load(PathConstants.MODEL_PATH, weights_only=True)
+    model.load_state_dict(state_dict)
 
-
+    inferencing = ModelInference(model=model,test_loader=test_loader)
+    hist = inferencing.infer()
+    print(hist)
 
