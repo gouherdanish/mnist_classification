4c4a384c18fb1516499707c9dd72240a99081e45
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-10 09:44:25 +0530

    renamed model folder

---
+++ model/mlp.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+from constants import Constants, ModelParams
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP,self).__init__()
+        self.image_pixels = Constants.IMAGE_SIZE[0]*Constants.IMAGE_SIZE[1]
+        self.fc1 = nn.Linear(self.image_pixels, ModelParams.FC1_NEURONS)
+        self.relu = nn.ReLU()
+        self.output = nn.Linear(ModelParams.FC1_NEURONS, ModelParams.OUTPUT_CLASSES)
+    
+    def forward(self, x):
+        x = x.view(-1,self.image_pixels)
+        x = self.relu(self.fc1(x))
+        return self.output(x)
