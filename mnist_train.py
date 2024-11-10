5af625bad6b06788b285a7a069784224a1148aea
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-10 09:48:17 +0530

    modified training main file

--- mnist_train.py
+++ mnist_train.py
@@ -1,22 +1,16 @@
 import argparse
 import torch
-import torch.nn as nn
 
-from constants import Constants, HyperParams, PathConstants
-from data.training_data import TrainingDataPreparation
+from constants import PathConstants
+from prep.training_data_prep_batch import BatchTrainingDataPreparation
 from factory.model_factory import ModelFactory
-from training import ModelTraining
+from pipeline.training import ModelTraining
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_type',type=str,default='mlp',choices=['mlp','lenet'],help='type of model to run on')
-    parser.add_argument('--epochs',type=int,default=10,help='number of epochs to train for')
-    args = parser.parse_args()
-
+def run(args):
     model_type = args.model_type
     epochs = args.epochs
 
-    data_prep = TrainingDataPreparation()
+    data_prep = BatchTrainingDataPreparation()
     train_loader, val_loader = data_prep.prepare()
 
     model_factory = ModelFactory()
@@ -30,4 +24,12 @@ if __name__=='__main__':
     print(hist)
 
     torch.save(model.state_dict(),PathConstants.MODEL_PATH)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_type',type=str,default='mlp',choices=['mlp','lenet'],help='type of model to run on')
+    parser.add_argument('--epochs',type=int,default=10,help='number of epochs to train for')
+    args = parser.parse_args()
+
+    run(args)
 
