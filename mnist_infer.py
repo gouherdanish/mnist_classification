8b1bc0fef343f0d68f35de4c291067914d5ec9e8
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-10 09:48:45 +0530

    Included capability to run batch and incremental inferences

--- mnist_infer.py
+++ mnist_infer.py
@@ -1,29 +1,45 @@
 import argparse
 import torch
-import torch.nn as nn
+from torchsummary import summary
 
-from constants import Constants, HyperParams, PathConstants
-from data.test_data import TestDataPreparation
+from constants import PathConstants
 from factory.model_factory import ModelFactory
-from inference import ModelInference
+from data.test_data_prep_batch import BatchTestDataPreparation
+from data.test_data_prep_incremental import IncrementalTestDataPreparation
+from model.batch_inference import BatchInference
+from model.incremental_inference import IncrementalInference
+from utils import Utils
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_type',type=str,default='mlp',choices=['mlp','lenet'],help='type of model to run on')
-    args = parser.parse_args()
-
+@Utils.timeit
+def run_pipeline(args):
     model_type = args.model_type
-
-    data_prep = TestDataPreparation()
-    test_loader = data_prep.prepare()
+    inference_strategy = args.inference_strategy
+    test_image_path = args.test_image_path
 
     model_factory = ModelFactory()
     model = model_factory.select(model_type)
     state_dict = torch.load(PathConstants.MODEL_PATH, weights_only=True)
     model.load_state_dict(state_dict)
 
-    inferencing = ModelInference(model=model,test_loader=test_loader)
-    hist = inferencing.infer()
+    if inference_strategy == 'incremental_inference' or test_image_path != '':
+        data_prep = IncrementalTestDataPreparation(test_image_path=test_image_path)  
+        inferencing = IncrementalInference(model=model)
+    else: 
+        data_prep = BatchTestDataPreparation()
+        inferencing = BatchInference(model=model)
+    test_loader = data_prep.prepare()
+    print(len(test_loader))
+
+    hist = inferencing.infer(test_loader=test_loader)
     print(hist)
     print(sum(hist['test_acc'])/len(hist['test_acc']))
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_type',type=str,default='mlp',choices=['mlp','lenet'],help='type of model to run on')
+    parser.add_argument('--inference_strategy',type=str,default='batch_inference',choices=['batch_inference','incremental_inference'],help='how to run inference - per image or per batch of image')
+    parser.add_argument('--test_image_path',type=str,default='',help='path of test image in case of incremental inference')
+    args = parser.parse_args()
+
+    run_pipeline(args)
 
