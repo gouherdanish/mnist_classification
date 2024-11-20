f28d855169c7797d5a6a9dcadabf64c5f3f52ffd
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-20 16:40:42 +0530

    added data files

--- data/batch_data.py
+++ data/batch_data.py
@@ -1,8 +1,9 @@
 import torchvision 
 
 class BatchData:
-    def __init__(self,train=True) -> None:
+    def __init__(self,train=True,transform=None) -> None:
         self.train=train
+        self.transform=transform
 
     def load(self):
         if self.train:
