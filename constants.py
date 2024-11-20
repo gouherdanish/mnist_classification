1771507378fda9807ec737139a4ea2c07791e420
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-20 16:43:39 +0530

    modified constants file

--- constants.py
+++ constants.py
@@ -1,17 +1,28 @@
 import os
 
-class Constants:
-    IMAGE_SIZE = (28,28)
-    VALIDATION_SPLIT_FRAC = 0.1
+class DataConstants:
+    IMAGE_SIZE = (28,28)            # size of input image
+    IN_CHANNELS = 1                 # grayscale
+    VALIDATION_SPLIT_FRAC = 0.1     # 10% validation set out of training set
+    OUTPUT_CLASSES = 10             # Labels 0 to 9
 
 class PathConstants:
     CUR_DIR = os.path.dirname(__file__)
-    MODEL_PATH = os.path.join(CUR_DIR,'out','mnist_mlp_weights.pth')
+
+    def MODEL_PATH(model):
+        return os.path.join(PathConstants.CUR_DIR,'out',f'mnist_{model}_weights.pth')
 
 class HyperParams:
-    LEARNING_RATE = 0.001
-    BATCH_SIZE = 128
+    LEARNING_RATE = 0.001           # Constant Learning rate
+    BATCH_SIZE = 128                # One batch has 128 images
 
-class ModelParams:
-    FC1_NEURONS = 512
-    OUTPUT_CLASSES = 10
+class MLPModelParams:
+    NEURONS_FC1 = 512               # Only one hidden layer of 512 neurons
+
+class LeNetModelParams:
+    NUM_FILTERS_CONVLAYER1 = 6
+    NUM_FILTERS_CONVLAYER2 = 16
+    KERNEL_SIZE_CONVLAYER = 5
+    KERNEL_SIZE_POOLINGLAYER = 2
+    NEURONS_FC1 = 120
+    NEURONS_FC2 = 84
