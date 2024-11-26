02f4c47594cc96fa8eb8b5414300015e8a073613
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-26 21:41:11 +0530

    modified model evaluator code

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -1,22 +1,22 @@
 import torch.nn as nn
 
 class ModelEvaluator:
-    def __init__(self,model:nn.Module) -> None:
-        self.model = model
-        self.result = {}
-
-    def count_params(self):
-        return sum([p.numel() for p in self.model.parameters() if p.requires_grad])
+    @staticmethod
+    def count_params(model):
+        return sum([p.numel() for p in model.parameters() if p.requires_grad])
     
-    def count_flops(self):
+    @staticmethod
+    def count_flops(model):
         total_flops = 0
-        for layer in self.model.children():
+        for layer in model.children():
             if isinstance(layer,nn.Linear):
                 flops_matmul = layer.in_features * layer.out_features
                 flops_bias = layer.out_features
                 total_flops += flops_matmul + flops_bias
         return total_flops
 
-    
-    def evaluate(self):
-        self.result['param_count'] = self.count_params()
+    def evaluate(self,model:nn.Module):
+        result = {}
+        result['param_count'] = self.count_params(model)
+        result['flops'] = self.count_flops(model)
+        return result
