5496bb8437b3c47bfc4202693bfd55f4c343f019
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-11-28 09:21:14 +0530

    added training params

---
+++ data/training_data.py
@@ -0,0 +1,11 @@
+from model_data import ModelData
+
+class TrainingData(ModelData):
+    def shuffle_flag(self):
+        return True
+    
+    def train_flag(self):
+        return True
+    
+    def download_flag(sself):
+        return True
