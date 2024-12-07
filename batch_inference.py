c8d4b819c0825a49cab5e663c91ce2da84bd38c6
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-12-07 23:58:34 +0530

    batch inference pipeline modified to include model checkpoint

--- batch_inference.py
+++ batch_inference.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import argparse
 import torch
 
@@ -13,18 +14,22 @@ def run(args):
     model_name = args.model_name
     inference_strategy = 'batch'
 
+    checkpoint_path = PathConstants.MODEL_PATH(model_name)
+    assert Path(checkpoint_path).exists(), f"Path Not Found: {checkpoint_path}"
+    checkpoint = torch.load(checkpoint_path, weights_only=True)
+
     model_factory = ModelFactory()
     model = model_factory.select(model_name)
-    state_dict = torch.load(PathConstants.MODEL_PATH(model_name), weights_only=True)
-    model.load_state_dict(state_dict)
+    model.load_state_dict(checkpoint['model_state'])
+
+    data_prep = BatchTestDataPreparation()
+    test_loader = data_prep.prepare()
 
     evaluator = ModelEvaluator()
     eval_result = evaluator.evaluate(model)
     print(eval_result)
 
-    inferencing = InferenceFactory.get(inference_strategy,model=model)
-    data_prep = BatchTestDataPreparation()
-    test_loader = data_prep.prepare()
+    inferencing = InferenceFactory.get(strategy=inference_strategy,model=model)
     hist = inferencing.infer(test_loader=test_loader)
     print(hist)
     print(sum(hist['test_acc'])/len(hist['test_acc']))
