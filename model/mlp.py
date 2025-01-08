89cbebae3a5436fe765031c23d436475960f60d2
Gouher Danish <gouherdanishiitkgp@gmail.com>
2025-01-08 09:37:33 +0530

    Added multiple MLP models for testing effects of model depth and width

--- model/mlp.py
+++ model/mlp.py
@@ -3,30 +3,47 @@ import torch.nn.functional as F
 
 from constants import DataConstants, MLPModelParams
 
-# class MLP(nn.Module):
-#     def __init__(self):
-#         super(MLP,self).__init__()
-#         self.model_name = 'mlp'
-#         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
-#         self.fc1 = nn.Linear(self.image_pixels, MLPModelParams.NEURONS_FC1)
-#         self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1, DataConstants.OUTPUT_CLASSES)
-    
-#     def forward(self, x):
-#         x = x.view(-1,self.image_pixels)
-#         x = F.relu(self.fc1(x))
-#         return self.fc2(x)
-
 class MLP(nn.Module):
     def __init__(self):
         super(MLP,self).__init__()
         self.model_name = 'mlp'
         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
-        self.fc1 = nn.Linear(self.image_pixels, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, DataConstants.OUTPUT_CLASSES)
+        self.fc1 = nn.Linear(self.image_pixels, 1024)
+        self.fc2 = nn.Linear(1024, DataConstants.OUTPUT_CLASSES)
     
     def forward(self, x):
         x = x.view(-1,self.image_pixels)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        return self.fc2(x)
+
+# class MLP(nn.Module):
+#     def __init__(self):
+#         super(MLP,self).__init__()
+#         self.model_name = 'mlp'
+#         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
+#         self.fc1 = nn.Linear(self.image_pixels, 512)
+#         self.fc2 = nn.Linear(512, 512)
+#         self.fc3 = nn.Linear(512, DataConstants.OUTPUT_CLASSES)
+    
+#     def forward(self, x):
+#         x = x.view(-1,self.image_pixels)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         return self.fc3(x)
+    
+# class MLP(nn.Module):
+#     def __init__(self):
+#         super(MLP,self).__init__()
+#         self.model_name = 'mlp'
+#         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
+#         self.fc1 = nn.Linear(self.image_pixels, 512)
+#         self.fc2 = nn.Linear(512, 512)
+#         self.fc3 = nn.Linear(512, 512)
+#         self.fc4 = nn.Linear(512, DataConstants.OUTPUT_CLASSES)
+    
+#     def forward(self, x):
+#         x = x.view(-1,self.image_pixels)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = F.relu(self.fc3(x))
+#         return self.fc4(x)
