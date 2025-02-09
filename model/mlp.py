b5f18f5255cbfd85bd2f6554b3f61693102ab1e9
Gouher Danish <gouherdanishiitkgp@gmail.com>
2025-02-09 23:13:08 +0530

    Added MLP-5 model arch

--- model/mlp.py
+++ model/mlp.py
@@ -3,18 +3,18 @@ import torch.nn.functional as F
 
 from constants import DataConstants, MLPModelParams
 
-class MLP(nn.Module):
-    def __init__(self):
-        super(MLP,self).__init__()
-        self.model_name = 'mlp'
-        self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
-        self.fc1 = nn.Linear(self.image_pixels, MLPModelParams.NEURONS_FC1)
-        self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1, DataConstants.OUTPUT_CLASSES)
+# class MLP(nn.Module):
+#     def __init__(self):
+#         super(MLP,self).__init__()
+#         self.model_name = 'mlp'
+#         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
+#         self.fc1 = nn.Linear(self.image_pixels, MLPModelParams.NEURONS_FC1)
+#         self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1, DataConstants.OUTPUT_CLASSES)
     
-    def forward(self, x):
-        x = x.view(-1,self.image_pixels)
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+#     def forward(self, x):
+#         x = x.view(-1,self.image_pixels)
+#         x = F.relu(self.fc1(x))
+#         return self.fc2(x)
 
 # class MLP(nn.Module):
 #     def __init__(self):
@@ -37,9 +37,9 @@ class MLP(nn.Module):
 #         self.model_name = 'mlp'
 #         self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
 #         self.fc1 = nn.Linear(self.image_pixels, MLPModelParams.NEURONS_FC1)
-#         self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1), MLPModelParams.NEURONS_FC2)
-#         self.fc3 = nn.Linear(MLPModelParams.NEURONS_FC2), MLPModelParams.NEURONS_FC3)
-#         self.fc4 = nn.Linear(MLPModelParams.NEURONS_FC3), DataConstants.OUTPUT_CLASSES)
+#         self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1, MLPModelParams.NEURONS_FC2)
+#         self.fc3 = nn.Linear(MLPModelParams.NEURONS_FC2, MLPModelParams.NEURONS_FC3)
+#         self.fc4 = nn.Linear(MLPModelParams.NEURONS_FC3, DataConstants.OUTPUT_CLASSES)
     
 #     def forward(self, x):
 #         x = x.view(-1,self.image_pixels)
@@ -47,3 +47,22 @@ class MLP(nn.Module):
 #         x = F.relu(self.fc2(x))
 #         x = F.relu(self.fc3(x))
 #         return self.fc4(x)
+    
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP,self).__init__()
+        self.model_name = 'mlp'
+        self.image_pixels = DataConstants.IMAGE_SIZE[0]*DataConstants.IMAGE_SIZE[1]
+        self.fc1 = nn.Linear(self.image_pixels, MLPModelParams.NEURONS_FC1)
+        self.fc2 = nn.Linear(MLPModelParams.NEURONS_FC1, MLPModelParams.NEURONS_FC2)
+        self.fc3 = nn.Linear(MLPModelParams.NEURONS_FC2, MLPModelParams.NEURONS_FC3)
+        self.fc4 = nn.Linear(MLPModelParams.NEURONS_FC3, MLPModelParams.NEURONS_FC4)
+        self.fc5 = nn.Linear(MLPModelParams.NEURONS_FC4, DataConstants.OUTPUT_CLASSES)
+    
+    def forward(self, x):
+        x = x.view(-1,self.image_pixels)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        return self.fc5(x)
